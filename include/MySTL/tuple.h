9cf4b1b461c5a3ef08bf925d07d993008821d62f
John <yangtianzhuo970425@gmail.com>
2025-03-12 23:59:29 +1300

    Implement additional tuple functionalities: for_each, find_if, find, count_if, count, append, and prepend

--- include/MySTL/tuple.h
+++ include/MySTL/tuple.h
@@ -9,6 +9,27 @@
 namespace My::MySTL {
 template <bool... Masks, typename Tuple, typename Init, typename Func>
 constexpr auto tuple_accumulate(Tuple&&, Init&&, Func&&);
+
+template <bool... Masks, typename Tuple, typename Func>
+constexpr void tuple_for_each(Tuple&&, Func&&);
+
+template <typename Tuple, typename Func>
+constexpr size_t tuple_find_if(const Tuple&, Func&&);
+
+template <typename Tuple, typename Elem>
+constexpr size_t tuple_find(const Tuple&, const Elem&);
+
+template <typename Tuple, typename Func>
+constexpr size_t tuple_count_if(const Tuple&, Func&&);
+
+template <typename Tuple, typename Elem>
+constexpr size_t tuple_count(const Tuple&, const Elem&);
+
+template <typename Tuple, typename... Elems>
+constexpr auto tuple_append(Tuple&&, Elems&&...);
+
+template <typename Tuple, typename... Elems>
+constexpr auto tuple_prepend(Tuple&&, Elems&&...);
 }  // namespace My::MySTL
 
 #include "detail/tuple.inl"
